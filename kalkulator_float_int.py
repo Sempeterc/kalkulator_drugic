345fe8e9d748d7e1800ad56793778920144059d5
Sempeterc <bostjangrmek@gmail.com>
2016-05-08 14:57:21 +0200

    Added files via upload

---
+++ kalkulator_float_int.py
@@ -0,0 +1,32 @@
+
+a_raw = raw_input ("vnesi stevilo A:")
+b_raw = raw_input("vnesi stevilo B:")
+
+
+operacija = raw_input("Operacija: ")
+
+if (a_raw.find(".") == -1):
+    a = int(a_raw)
+else:
+    a = float(a_raw)
+
+if (b_raw.find(".") == -1):
+    b = int(b_raw)
+else:
+    b = float(b_raw)
+
+if (operacija == "+"):
+    rezultat = a + b
+elif (operacija == "-"):
+    rezultat = a - b
+elif (operacija == "*"):
+    rezultat = a * b
+elif (operacija == "/"):
+    rezultat = a / b
+else:
+    print "Te operacije ne poznam"
+
+if 'rezultat' in locals(): # ali spremenljivka rezultat obstaja
+    print "Rezultat a ("\
+          + str(a) + ") " + operacija\
+          + " b (" + str(b) + ") = " + str(rezultat)
